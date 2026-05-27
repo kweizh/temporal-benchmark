7736f7eccb85c27e31e42149823247d273a73d8f
Wei Zhang <kweizh@tabbyml.com>
2026-05-27 10:25:46 +0800

    fix(jobs): drop wrong jobs for flash

--- jobs/2026-05-23__15-22-33/parallel_activities_workflow_py__HVJLrhZ/artifacts/user/myproject/activities.py
+++
@@ -1,8 +0,0 @@
-import httpx
-from temporalio import activity
-
-@activity.defn
-async def fetch_url(url: str) -> int:
-    async with httpx.AsyncClient() as client:
-        response = await client.get(url)
-        return response.status_code
